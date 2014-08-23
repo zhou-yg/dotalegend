3d7ad0d9312a2fc8093129060dc8d40dfc376a17
Joy Youg <zhou.y.g890@gmail.com>
2014-08-23 11:02:27 +0800

    初始化布局2

---
+++ excute.js
@@ -0,0 +1,24 @@
+var allWidth = $(document).width();
+var allHeight = $(document).height();
+//init
+$(document).ready(function(){
+
+	var $set  = $('.setting'); 
+	var $main = $('.main');
+
+	$main.height(allHeight);
+	$set.height(allHeight);
+
+	$set.show();
+});
+//操作
+$(document).ready(function(){
+	
+	var $opsGold   = $('.main .ops_gold');
+	var $opsGold   = $('.main .ops_equipment');
+});
+//设置
+$(document).ready(function(){
+	
+	var $set_select = $('.setting .set_select');
+});
