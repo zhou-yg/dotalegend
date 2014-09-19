c0282e0fbebb07671eb87d6211ecb36e54af40e5
= <=>
2014-09-19 19:39:45 +0800

    a little

--- Js/oop/dget.js
+++ Js/oop/dget.js
@@ -1,9 +1,9 @@
-var serverHost = 'http://10.0.1.82:9000/dotalegend_git';
+var serverHost = 'http://10.0.1.82:9000/dotalegend_git/dotalegend';
 
 var dotalegendGet = {
 	
 	getAllHeroes:function(_callBack){
-		$.get(serverHost+'/dotalegend/phps2/getAllHeroes.php',function(_data,_result){
+		$.get(serverHost+'/phps2/getAllHeroes.php',function(_data,_result){
 
 			if(typeof _data == 'object'){
 
@@ -15,7 +15,7 @@ var dotalegendGet = {
 		});
 	},
 	getHero:function(_sendData,_callBack){
-		$.get(serverHost+'/dotalegend/phps2/getHero.php?heroNamesJson='+_sendData,function(_data,_result){
+		$.get(serverHost+'/phps2/getHero.php?heroNamesJson='+_sendData,function(_data,_result){
 
 			if(typeof _data == 'object'){
 
